dfabfcc6412490dbc53166b051e4789534bd7ef2
Vishesh Patil <patil.cloud11@gmail.com>
2026-02-02 12:59:03 +0530

    Refactor login tests to store results in variables

--- src/test/java/com/project/AppTest.java
+++ src/test/java/com/project/AppTest.java
@@ -8,17 +8,33 @@ public class AppTest {
     @Test
     public void testLogin1() {
         App myApp = new App();
-        Assert.assertEquals(0, myApp.userLogin("abc", "abc123"));
+        int result = myApp.userLogin("abc", "abc123");
+
+        System.out.println("testLogin1 -> Username: abc, Password: abc123");
+        System.out.println("Expected Result: 0, Actual Result: " + result);
+
+        Assert.assertEquals(result, 0);
     }
 
     @Test
     public void testLogin2() {
         App myApp = new App();
-        Assert.assertEquals(1, myApp.userLogin("abc", "abc@123"));
+        int result = myApp.userLogin("abc", "abc@123");
+
+        System.out.println("testLogin2 -> Username: abc, Password: abc@123");
+        System.out.println("Expected Result: 1, Actual Result: " + result);
+
+        Assert.assertEquals(result, 1);
     }
-     @Test
+
+    @Test
     public void testLogin3() {
         App myApp = new App();
-        Assert.assertEquals(1, myApp.userLogin("abc", "abc@123"));
+        int result = myApp.userLogin("abc", "abc@123");
+
+        System.out.println("testLogin3 -> Username: abc, Password: abc@123");
+        System.out.println("Expected Result: 1, Actual Result: " + result);
+
+        Assert.assertEquals(result, 1);
     }
 }
